3e65cebefcb5546f806a7c64db56ca02d8a58cd9
viowelyas <69565900+viowelyas@users.noreply.github.com>
2022-05-21 10:49:03 +0700

    init commit

---
+++ api/product/detail.php
@@ -0,0 +1,32 @@
+<?php 
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+
+  include_once '../../config/Database.php';
+  include_once '../../models/Product.php';
+
+  // Instantiate DB & connect
+  $database = new Database();
+  $db = $database->connect();
+
+  // Instantiate blog post object
+  $post = new Post($db);
+
+  // Get ID
+  $post->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+  // Get post
+  $post->read_single();
+
+  // Create array
+  $post_arr = array(
+    'id' => $post->id,
+    'name' => $post->name,
+    'uom' => $post->uom,
+    'stock' => $post->stock,
+    'price' => $post->price
+  );
+
+  // Make JSON
+  print_r(json_encode($post_arr));
